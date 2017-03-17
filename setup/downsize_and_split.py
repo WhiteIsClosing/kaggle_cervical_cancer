04e38d43eef20628f9349fe3ba37ce391e09de66
Yuelong Guo <ylguo0716@gmail.com>
2017-03-16 22:57:38 -0400

    added scripts to process additioanl images

--- setup/downsize_and_split.py
+++ setup/downsize_and_split.py
@@ -9,7 +9,7 @@ def resize(img_path):
 
 ### Decide whether the image gets saved to train or valid ###
 ### There is better ways to implement this, but since only run once ###
-def save_img_to_path(img_path, img, typ):
+def save_train_img_to_path(img_path, img, typ):
     img_name = os.path.basename(img_path) 
     if(typ not in ["Type_1", "Type_2", "Type_3"]):
         sys.exit("Invalid typ argument")
@@ -20,21 +20,59 @@ def save_img_to_path(img_path, img, typ):
         new_path = os.path.join("../data/train", typ, img_name)
     imsave(new_path, img)
 
-### main ###
-if __name__ == "__main__":
-    ### loop through image and process
-    ### train
+### Decide whether the additional image gets saved to train or valid ###
+def save_addl_img_to_path(img_path, img, typ):
+    img_name = os.path.basename(img_path) 
+    if(typ not in ["Type_1", "Type_2", "Type_3"]):
+        sys.exit("Invalid typ argument")
+    valid = open(typ + ".additional.txt").read().split()
+    if(img_name in valid):
+        new_path = os.path.join("../data/additional/valid", typ, img_name)
+    else:
+        new_path = os.path.join("../data/additional/train", typ, img_name)
+    imsave(new_path, img)
+
+### train
+def process_train():
     for typ in ["Type_1", "Type_2", "Type_3"]:
         for f in os.listdir(os.path.join("../download/train/", typ)):
             if f.endswith("jpg"):
                 impath = os.path.join("../download/train/", typ, f)
                 print "processing " + impath
-                img = resize(impath)
-                save_img_to_path(impath, img, typ)
-    ### test 
+                try:
+                    img = resize(impath)
+                    save_train_img_to_path(impath, img, typ)
+                except:
+                    pass
+
+### test 
+def process_test():
     for f in os.listdir(os.path.join("../download/test/")):
         if f.endswith("jpg"):
             impath = os.path.join("../download/test/", f)
             print "processing " + impath
-            img = resize(impath)
-            imsave(os.path.join("../data/test/unknown/", os.path.basename(impath)), img)
+            try:
+                img = resize(impath)
+                imsave(os.path.join("../data/test/unknown/", os.path.basename(impath)), img)
+            except:
+                pass
+
+### additional 
+def process_additional():
+    for typ in ["Type_1", "Type_2", "Type_3"]:
+        for f in os.listdir(os.path.join("../download/additional/", typ)):
+            if f.endswith("jpg"):
+                impath = os.path.join("../download/additional/", typ, f)
+                print "processing " + impath
+                try:
+                    img = resize(impath)
+                    save_addl_img_to_path(impath, img, typ)
+                except:
+                    pass
+
+### main ###
+if __name__ == "__main__":
+    ### loop through image and process
+    # process_train()
+    # process_test()
+    process_additional()
--- setup/setup.sh
+++ setup/setup.sh
@@ -2,10 +2,13 @@
 
 ### Generate list of valid images ###
 # python generate_split_list.py
+# python generate_split_list_additional.py
 
 ### Directory to save the processed data ###
 mkdir -p ../data/train/Type_1/ ../data/train/Type_2/ ../data/train/Type_3/
 mkdir -p ../data/valid/Type_1/ ../data/valid/Type_2/ ../data/valid/Type_3/
+mkdir -p ../data/additional/train/Type_1/ ../data/additional/train/Type_2/ ../data/additional/train/Type_3/
+mkdir -p ../data/additional/valid/Type_1/ ../data/additional/valid/Type_2/ ../data/additional/valid/Type_3/
 mkdir -p ../data/test/unknown/
 
 ### Preprocess image and split into train and valid ###
